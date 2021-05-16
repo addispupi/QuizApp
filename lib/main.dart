30eb091c9b0ed9ed93c389c21875dac2ed55f033
Addisu <pupiatpixar@gmail.com>
2021-05-16 20:31:19 +0300

    'home : initiating level'

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyQuiz());
+
+class MyQuiz extends StatelessWidget {
+  @override
+  Widget build(BuildContext contex) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Intelligence Questions'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
+  }
+}
