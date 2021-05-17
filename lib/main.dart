3b958ac83269916ee8827c29493f798ec9fb69cb
Addisu <pupiatpixar@gmail.com>
2021-05-18 01:18:45 +0300

    'Stateful Widget'

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyQuiz());
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return QuizAppState();
+  }
 }
 
-class MyQuiz extends StatelessWidget {
+class QuizAppState extends State<QuizApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(title: Text('መልመጃ ደብተር')),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('answer 2 choosen')),
+            ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Answer 3 chooen');
+                }),
+          ],
+        ),
       ),
     );
   }
 }
-
-// void main() => runApp(MyQuiz());
-
-// class MyQuiz extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext contex) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Intelligence Questions'),
-//         ),
-//         body: Text('This is my default text!'),
-//       ),
-//     );
-//   }
-// }
