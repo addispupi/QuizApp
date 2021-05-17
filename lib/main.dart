384db518ed7be7fa9a67e338026721ce34133902
Addisu <pupiatpixar@gmail.com>
2021-05-17 22:35:33 +0300

    'Update - Second Day'

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyQuiz());
+void main() {
+  runApp(MyQuiz());
+}
 
 class MyQuiz extends StatelessWidget {
-  @override
-  Widget build(BuildContext contex) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Intelligence Questions'),
-        ),
-        body: Text('This is my default text!'),
+        appBar: AppBar(),
       ),
     );
   }
 }
+
+// void main() => runApp(MyQuiz());
+
+// class MyQuiz extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext contex) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Intelligence Questions'),
+//         ),
+//         body: Text('This is my default text!'),
+//       ),
+//     );
+//   }
+// }
