9eea6690a396253a29f40a3acf9863f3bd4028ea
Addisu <pupiatpixar@gmail.com>
2021-05-26 21:57:47 +0300

    'Text decoration'

--- lib/question.dart
+++ lib/question.dart
@@ -6,6 +6,14 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(6),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 25),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
