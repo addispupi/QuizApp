94a5241ca42bfdd6f24ee47eb4bba63d156343b9
Addisu <pupiatpixar@gmail.com>
2021-06-06 19:20:16 +0300

    'Assets UI - Updated'

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const wPrimaryColor = Color(0xFF1565C0);
+const wPrimaryLightColor = Color(0xFF64B5F6);
+const wPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF1565C0), Color(0xFF64B5F6)],
+);
+const wSecondaryColor = Color(0xFFFF6D00);
+const wSecondaryLightColor = Color(0xFFF57C00);
+const wTextColor = Color(0xFF212121);
+const wTextColorLight = Color(0xFF616161);
+const wAnimationDuration = Duration(milliseconds: 200);
